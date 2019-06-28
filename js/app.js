706062c5911bc785d4a92f7e1ace66e47dffa0eb
Luke McGeath <49741190+lmcgeath@users.noreply.github.com>
2019-06-27 19:58:36 -0700

    final
    Final project to be turned in

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 let selectT = $('#color').find('option[value="Please select a T-shirt theme"]').val();
+//creates a variable of the list of checkboxes to loop over in the activities section
+const checkboxes = $('.activities input');
 
-//NEED TO PREVENT FIREFOX FROM SAVING CHECKED BOXES
 //Focuses cursor on first text box
 $('#name').focus();
 
@@ -10,14 +11,12 @@ $('#other-title').hide();
 when other option is selected*/
 $('#title').on('change', function(){
   if ($(this).val() == 'other'){
-    $('#other-title').show()
+    $('#other-title').show();
   }
   else {
     $('#other-title').hide();
-
   }
 });
-
 //------------------T shirt section -------------------------------
 
 //Hides colors on first page load
@@ -52,11 +51,11 @@ $('.shirt').change(function(){
 //------------------Activity section -------------------------------
 //establishes counter for to keep track of event costs
 let totalCost = 0;
-//creates new <p> element and adds text plus the varable for totalCost
+//creates new <p> element and adds text plus the variable for totalCost
 let newElement = $('<p></p>');
 //appends new element to the bottom of the list of event checkboxes
 $('.activities').append(newElement);
-
+//creates a change event handler
 $('.activities').change(function(){
   const lastBoxChecked = event.target;
   const labelText = $(lastBoxChecked).parent().text();
@@ -74,25 +73,13 @@ accordingly*/
 }
  //sets the text of the event cost counter to display as it changes
  newElement.text('Total: $' + totalCost);
-
+//creates variables to access the time and day of event
  const dashIndex = labelText.indexOf('—');
  const commaIndex = labelText.indexOf(',');
  const timeOfEvent = labelText.slice(dashIndex, commaIndex);
- // console.log(dashIndex)
- // console.log(commaIndex)
- // console.log(timeOfEvent)
-
- //creates a variable of the list of checkboxes to loop over -- MAY NEED TO EDIT
- const checkboxes = $('.activities input');
- // console.log(labelText)
-
+//creates a loop to check for conflicting events and disable them
  for (i = 0; i<checkboxes.length; i++) {
-   // let lastBoxText = lastBoxChecked.text()
    let checkboxText = $(checkboxes[i]).parent().text();
-   // let lastTimeOfEvent = timeOfEvent[i]
-   // console.log(lastTimeOfEvent)
-   // if($(checkboxes).prop('checked') == true && lastTimeOfEvent == timeOfEvent)
-
    if(checkboxText.includes(timeOfEvent) && checkboxText !== labelText) {
      if ( $(lastBoxChecked).prop('checked') === false ) {
 				checkboxes[i].disabled = false;
@@ -100,6 +87,155 @@ accordingly*/
 				checkboxes[i].disabled = true;
 			}
    }
-
  }
 });
+//------------------Payment section -------------------------------
+//hides select option value
+$('#payment option[value="select_method"]').hide();
+$('.paypal').hide();
+$('.bitcoin').hide();
+
+//defaults the first option to 'credit card' in the dropdown menu
+$('#payment').val('credit card');
+/*creates a change event to get the value of the selected option and show
+content accordingly*/
+$('#payment').change(function(){
+let paymentChoice = $(event.target).val();
+if (paymentChoice == 'paypal'){
+  $('.paypal').show();
+  $('.bitcoin').hide();
+  $('.credit-card').hide();
+  }
+else if (paymentChoice == 'bitcoin'){
+  $('.bitcoin').show();
+  $('.paypal').hide();
+  $('.credit-card').hide();
+}
+else if (paymentChoice == 'credit card'){
+  $('.bitcoin').hide();
+  $('.paypal').hide();
+  $('.credit-card').show();
+}
+});
+
+//------------------Validation section -------------------------------
+
+//Function for validating name field
+function validateName (name){
+  //captures user input into a variable
+  const username = $('#name').val();
+  if (username == "" || username == " "){
+    $('#name').css("border","2px solid red");
+    return false
+  }
+  else {
+    $('#name').css('border', '1px solid green');
+    return true
+  }
+}
+
+//Tests valid email regex expression against email input and colors box accordingly
+function validateEmail (){
+  let emailTest = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  let email = $('#mail').val();
+  if (emailTest.test(email) == false){
+    $('#mail').css("border","2px solid red");
+    return false
+  }
+  else {
+    $('#mail').css('border', '1px solid green');
+    return true
+}
+}
+//Function for validating event boxes checked
+function validateActivities(){
+  let numberOfChecked = $(checkboxes).filter(':checked').length
+  if (numberOfChecked == 0) {
+      $('.activities').css("border","2px solid red");
+      return false
+  }
+  else {
+    $('.activities').css('border', '1px solid green');
+    return true
+}
+}
+//Function for validating credit card input
+function validateCreditCard(){
+  let creditCardTest = /^\d{16}$/
+  creditCard = $('#cc-num').val();
+  if (creditCardTest.test(creditCard) == false){
+    $('#cc-num').css("border","2px solid red");
+    return false
+  }
+  else {
+    $('#cc-num').css('border', '1px solid green');
+    return true
+}
+};
+//Function for validating zip code input
+function validateZip(){
+  let zipTest = /^\d{5}$/
+  zipcode = $('#zip').val();
+  if (zipTest.test(zipcode) == false){
+    $('#zip').css("border","2px solid red");
+    return false
+  }
+  else {
+    $('#zip').css('border', '1px solid green');
+    return true
+}
+};
+//Function for validating cvv input
+function validateCvv(){
+  let cvvTest = /^\d{3}$/
+  cvv = $('#cvv').val();
+  if (cvvTest.test(cvv) == false){
+    $('#cvv').css("border","2px solid red");
+    return false
+  }
+  else {
+    $('#cvv').css('border', '1px solid green');
+    return true
+}
+};
+/*creates a function to run all of the validation functions and evaluate
+whether they are true or false to prevent the form from submitting in the
+handler below*/
+function validateAll(){
+  validateName(name);
+  validateEmail();
+  validateActivities();
+  validateCreditCard();
+  validateZip();
+  validateCvv();
+  let returnValue = true
+  if (validateName(name) == false){
+    returnValue = false
+  }
+  if(validateEmail() == false) {
+     returnValue = false;
+  }
+
+  if (validateActivities() == false) {
+     returnValue = false;
+   }
+  if ($("#payment").val() === 'credit card') {
+    if(validateCreditCard() == false) {
+        returnValue = false;
+        }
+    if(validateZip() == false) {
+        returnValue = false;
+        }
+    if (validateCvv() == false) {
+        returnValue = false;
+        }
+    }
+      return returnValue;
+};
+/*Event handler to run the validation functions upon user input or submitting the form.
+Also prevents the form from submitting until all input is valid*/
+$('form').on('input submit', function(){
+if (validateAll() == false){
+  event.preventDefault();
+validateAll()
+}});
